718cf924170629860e037abc29839a7535e2c994
Brian <infocapcollegeconsulting@gmail.com>
2020-08-09 11:42:33 -0700

    navbar responsive update

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-var x = 'hello'
+const hamburger = document.querySelector(".hamburger-btn");
+const navbar__items = document.querySelector(".navbar__items");
+const navbar__icons = document.querySelector(".navbar__icons");
+
+function show() {
+  navbar__items.classList.toggle("show");
+  navbar__icons.classList.toggle("show");
+}
+hamburger.addEventListener("click", show);
